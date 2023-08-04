5bc09e25e9fe90951bc74982140c05b4616359f2
idowilunski <idowilunski@gmail.com>
2023-08-04 18:59:47 +0300

    missing func in second pass

--- src/Instructions_struct.c
+++ src/Instructions_struct.c
@@ -39,6 +39,7 @@ void intialiez_ins_node(Ins_Node** head, command com, int param_type[2]) {
     (*head)->operrands[0] = param_type[0];
     (*head)->operrands[1] = param_type[1];
     (*head)->next = NULL;
+    (*head)->bin_rep = NULL;
 }
 
 void print_ins_node(Ins_Node* head){
