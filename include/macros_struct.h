011f0cbf591b1891791d6b0df02f2f53d5893eaa
Zoeshwa <zoe.shwartz@gmail.com>
2023-08-05 19:28:20 +0300

    ss

--- include/macros_struct.h
+++ include/macros_struct.h
@@ -1,14 +1,14 @@
-    #include "string_functions.h"
+#include "string_functions.h"
 
-    typedef struct Macro_Node Macro_Node;
+typedef struct Macro_Node Macro_Node;
 
-    void make_am_file(char* orig_name);
-    void add_extention(char* file_original_name, char* with_ext_name, char* ext);
-    void insertMacro_Node(struct Macro_Node** head, char* name);
-    void update_macro_contect(struct Macro_Node** head, char* line);
-    struct Macro_Node* search_macro_list(struct Macro_Node* head, char* name);
-    void free_macro_list(Macro_Node** head);
-    void print_macro_list(Macro_Node* head);
+void make_am_file(char* orig_name);
+void add_extention(char* file_original_name, char* with_ext_name, char* ext);
+void insertMacro_Node(struct Macro_Node** head, char* name);
+void update_macro_contect(struct Macro_Node** head, char* line);
+struct Macro_Node* search_macro_list(struct Macro_Node* head, char* name);
+void free_macro_list(Macro_Node** head);
+void print_macro_list(Macro_Node* head);
 
 
 
